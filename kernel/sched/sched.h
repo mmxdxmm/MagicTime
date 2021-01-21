383650adc9d8580c94e51e843426182be98e7638
Peter Zijlstra <peterz@infradead.org>
2021-01-21 16:09:32 +0100

    sched: Use cpu_dying() to fix balance_push vs hotplug-rollback
    Use the new cpu_dying() state to simplify and fix the balance_push()
    vs CPU hotplug rollback state.
    
    Specifically, we currently rely on notifiers sched_cpu_dying() /
    sched_cpu_activate() to terminate balance_push, however if the
    cpu_down() fails when we're past sched_cpu_deactivate(), it should
    terminate balance_push at that point and not wait until we hit
    sched_cpu_activate().
    
    Similarly, when cpu_up() fails and we're going back down, balance_push
    should be active, where it currently is not.
    
    So instead, make sure balance_push is enabled below SCHED_AP_ACTIVE
    (when !cpu_active()), and gate it's utility with cpu_dying().
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Valentin Schneider <valentin.schneider@arm.com>
    Link: https://lkml.kernel.org/r/YHgAYef83VQhKdC2@hirez.programming.kicks-ass.net

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1048,7 +1048,6 @@ struct rq {
 	unsigned long		cpu_capacity;
 
 	struct callback_head	*balance_callback;
-	unsigned char		balance_push;
 
 	unsigned char		nohz_idle_balance;
 	unsigned char		idle_balance;

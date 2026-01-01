aaf597db36469d2d5cd6783f7310a4867e89dd2c
李小东 <825346670@qq.com>
2026-01-01 15:51:15 +0800

    Add files via upload

--- build_test.sh
+++ build_test.sh
@@ -2,25 +2,25 @@
 
 set -e
 
-mkdir -p clang
-if [ -f "clang.tar.gz" ]; then
+if [ -f "android-ndk-r29.zip" ]; then
     echo "文件已存在，正在解压..."
-    yes | tar -xvf clang.tar.gz -C clang
+    yes | unzip android-ndk-r29.zip
 else
     echo "文件不存在，正在下载..."
-    wget -nv -O clang.tar.gz "https://github.com/mmxdxmm/aosp-clang/releases/download/r563880c/clang-r563880c.tar.gz"
+    wget -O android-ndk-r29.zip "https://dl.google.com/android/repository/android-ndk-r29-linux.zip"
     if [ $? -eq 0 ]; then
         echo "下载完成，正在解压..."
-        yes | tar -xvf clang.tar.gz -C clang
+        yes | unzip android-ndk-r29.zip
     else
         echo "下载失败，请检查网络或链接是否正确。"
     fi
 fi
 
-#yes | tar -xvf electron-binutils-2.41.tar.xz
+#wget -nv -O binutils.zip https://github.com/mmxdxmm/binutils/releases/download/20251013/x86-64_binutils-2.33.1.zip
+#yes | unzip binutils.zip
 yes | unzip change.zip
-TOOLCHAIN_PATH=$PWD/clang/bin
-#BINUTILS_PATH=$PWD/electron-binutils-2.41/bin
+TOOLCHAIN_PATH=$PWD/android-ndk-r29/toolchains/llvm/prebuilt/linux-x86_64/bin
+#BINUTILS_PATH=$PWD/binutils/bin
 GIT_COMMIT_ID="mmxdxmm"
 
 TARGET_DEVICE=$1
@@ -55,8 +55,8 @@ echo "CCACHE_DIR: [$CCACHE_DIR]"
 
 
 MAKE_ARGS="ARCH=arm64 SUBARCH=arm64 O=out LVM=1 LLVM_IAS=1 AR=llvm-ar NM=llvm-nm STRIP=llvm-strip OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump HOSTAR=llvm-ar"
-set_C="ccache clang --target=aarch64-linux-musl -Os -march=armv8.2-a+lse+crypto+dotprod -mcpu=cortex-a77 -flto=thin -Wno-error -ffunction-sections -fdata-sections"
-set_HOSTC="ccache clang -Os -flto=thin -Wno-error -ffunction-sections -fdata-sections"
+set_C="ccache clang -v --target=aarch64-linux-musl -Os -march=armv8.2-a+lse+crypto+dotprod -mcpu=cortex-a77 -flto=thin -Wno-error -ffunction-sections -fdata-sections"
+set_HOSTC="ccache clang -v -Os -flto=thin -Wno-error -ffunction-sections -fdata-sections"
 set_LD="ld.lld --strip-debug"
 set_HOSTLD="ld.lld --strip-debug --gc-sections"
 
@@ -182,7 +182,7 @@ scripts/config --file out/.config \
     -e CONFIG_THINLTO \
     -d CONFIG_CFI_CLANG
 
-make LD="$set_LD" HOSTLD="$set_HOSTLD" CC="$set_C" CXX="$set_C" HOSTCC="$set_HOSTC" HOSTCXX="$set_HOSTC" $MAKE_ARGS $MAKE_ARGS -j$(nproc)
+make LD="$set_LD" HOSTLD="$set_HOSTLD" CC="$set_C" CXX="$set_C" HOSTCC="$set_HOSTC" HOSTCXX="$set_HOSTC" $MAKE_ARGS -j$(nproc)
 
 
 

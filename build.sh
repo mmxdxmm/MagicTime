6fc3cfa8208fbe225a41a94372ec4ed21a4332ee
李小东 <1658011573@qq.com>
2026-07-23 06:49:47 +0800

    Add files via upload

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ if [ -f "clang.tar.gz" ]; then
     yes | tar -xvf clang.tar.gz -C clang
 else
     echo "文件不存在，正在下载..."
-    wget -nv -O clang.tar.gz "https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main-kernel/clang-r547379.tar.gz"
+    wget -nv -O clang.tar.gz "https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main-kernel/clang-r596125.tar.gz"
     if [ $? -eq 0 ]; then
         echo "下载完成，正在解压..."
         yes | tar -xvf clang.tar.gz -C clang
@@ -178,7 +178,8 @@ scripts/config --file out/.config \
     -e CONFIG_LD_DEAD_CODE_DATA_ELIMINATION \
     -e CONFIG_CPU_IDLE_GOV_TEO \
     -e CONFIG_THINLTO \
-    -d CONFIG_CFI_CLANG
+    -d CONFIG_CFI_CLANG \
+    -d QPNP_FG_GEN4
 
 make LD="$set_LD" HOSTLD="$set_HOSTLD" CC="$set_C" CXX="$set_C" HOSTCC="$set_HOSTC" HOSTCXX="$set_HOSTC" $MAKE_ARGS $MAKE_ARGS -j$(nproc)
 
